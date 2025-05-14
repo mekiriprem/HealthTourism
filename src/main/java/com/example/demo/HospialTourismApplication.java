48daad1ff9f57edc155d96f05e4c099d39d2e504
PremchanduMekiri <premmekiri22@gmail.com>
2025-05-14 12:07:49 +0530

    project

---
+++ src/main/java/com/example/demo/HospialTourismApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospialTourismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospialTourismApplication.class, args);
+	}
+
+}
