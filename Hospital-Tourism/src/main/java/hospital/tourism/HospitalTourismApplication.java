01a4c90dbe096d1529ee734a0cf4cf5e74be136f
AnilNaga <nagaanil0329@gmail.com>
2025-05-14 12:47:49 +0530

    COMMIT FROM ANIL

---
+++ Hospital-Tourism/src/main/java/hospital/tourism/HospitalTourismApplication.java
@@ -0,0 +1,13 @@
+package hospital.tourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalTourismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalTourismApplication.class, args);
+	}
+
+}
---
+++ Hospital-Tourism/src/test/java/hospital/tourism/HospitalTourismApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital.tourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalTourismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
