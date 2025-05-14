c8a9f86d38d54bd098f9926cc709b79bf13c7300
mekiriprem <premchandu.m@zynlogic.com>
2025-05-14 13:40:15 +0530

    proJ

---
+++ src/main/java/com/example/demo/Entity/users.java
@@ -0,0 +1,29 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+// or "patients"
+public class users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false, unique = true)
+    private long mobilenum;
+    
+    @Column(nullable = false, unique = true)
+    private String country;
+    
+    @Column(nullable = false)
+    private String password;
+
+    // Optional: role field for admin/patient
+    @Column(nullable = false)
+    private String role = "PATIENT";
+
+}
